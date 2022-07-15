00ca5002247fc220c77e43521dcb563fa360a579
Tiberio <leandrostiberio@gmail.com>
2022-07-14 23:23:01 -0300

    Implementando APIs em um fluxo

--- ProjetoAws.Web/Controllers/UsuarioController.cs
+++ ProjetoAws.Web/Controllers/UsuarioController.cs
@@ -4,6 +4,10 @@ using ProjetoAWS.Lib.Models;
 
 using Curso.ProjetoAWS.Lib.Data.Repositorios.Interface;
 using ProjetoAWS.Lib.Exceptions;
+using Amazon.S3;
+using Amazon.Rekognition;
+using Amazon.S3.Model;
+using Amazon.Rekognition.Model;
 
 namespace ProjetoAws.Web.Controllers
 {
@@ -12,10 +16,15 @@ namespace ProjetoAws.Web.Controllers
     public class UsuarioController : ControllerBase 
     {
         private readonly IUsuarioRepositorio _repositorio;
+        private readonly IAmazonS3 _amazonS3;
+        private readonly AmazonRekognitionClient _rekognitionClient;
+        private static readonly List<string> _extensoesImagem = new List<string>(){"imagejpeg", "imagepng"};
         public static List<Usuario> ListaUsuarios { get; set; } = new List<Usuario>();
-        public UsuarioController(IUsuarioRepositorio repositorio)
+        public UsuarioController(IUsuarioRepositorio repositorio, IAmazonS3 amazonS3, AmazonRekognitionClient rekognitionClient  )
         {
             _repositorio = repositorio;
+            _amazonS3 = amazonS3;
+            _rekognitionClient = rekognitionClient;
         }
 
         [HttpGet("Todos")]
@@ -43,6 +52,69 @@ namespace ProjetoAws.Web.Controllers
             catch (ErroDeValidacaoException ex)
             {
                 return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost("Cadastro Imagem")]
+        public async Task<IActionResult> CadastroDeImagem(int id, IFormFile imagem)
+        {
+            var nomeArquivo = await SalvarNoS3(imagem);
+            var imagemValida = await ValidarImagem(nomeArquivo);
+            if(imagemValida)
+            {   
+                await _amazonS3.PutBucketAsync(nomeArquivo);
+                return BadRequest();
+            }
+            else
+            {
+                await _amazonS3.DeleteObjectAsync("imagem-aulas", nomeArquivo);
+                return BadRequest();
+            }
+        }
+
+        private async Task<string> SalvarNoS3(IFormFile imagem)
+        {
+            if(!_extensoesImagem.Contains(imagem.ContentType))
+            {
+                throw  new Exception("Tipo Invalido de imagem");
+            }
+            using (var streamDaImagem = new MemoryStream())
+            {  
+                await imagem.CopyToAsync(streamDaImagem);  
+
+                var request = new PutObjectRequest();
+                request.Key = "reconhecimento" + imagem.FileName;
+                request.BucketName = "imagem-Aulas";
+                request.InputStream = streamDaImagem;
+                
+                var resposta = await _amazonS3.PutObjectAsync(request);
+                return request.Key;
+            }
+        }
+
+        private async Task<bool> ValidarImagem(string nomeArquivo)
+        {
+             var entrada = new DetectFacesRequest();
+            var imagem = new Image();
+
+            var s3Object = new Amazon.Rekognition.Model.S3Object()
+            {
+                Bucket = "imagem-Aulas",
+                Name = nomeArquivo
+            };
+
+            imagem.S3Object = s3Object;
+            entrada.Image = imagem;
+            entrada.Attributes = new List<string>(){"ALL"};
+            
+            var resposta = await _rekognitionClient.DetectFacesAsync(entrada);
+            
+            if(resposta.FaceDetails.Count == 1 && resposta.FaceDetails.First().Eyeglasses.Value == false)
+            {
+                return true;
+            }
+            else
+            {
+                return true;
             }
         }
 
